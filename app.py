c2c60cb550c4b025dad32b635a4f278b9157b96c
Ahmed Ashraf <a.a.elghawas@gmail.com>
2023-07-18 01:58:26 +0300

    Graph is update with user input + error messages

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import sys
 import matplotlib
+import numpy as np
+from sympy import SympifyError, var
+from sympy import sympify
 matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
-from PySide2.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout
+from PySide2.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QMessageBox
 from PySide2.QtCore import Qt, QSize
 from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import (
@@ -28,6 +31,8 @@ from PySide2.QtWidgets import (
     QVBoxLayout,
     QWidget,
 )
+
+LS = 1000
 class MainWindow(QMainWindow):
 
     def __init__(self):
@@ -36,31 +41,44 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Function Plotter")
 
         self.setFixedSize(QSize(600, 500))
+        self.syExpr = '' # for parsing user input function
 
         layout1 = QHBoxLayout()
         layout2 = QVBoxLayout() 
 
         # Create the maptlotlib FigureCanvas object,
         # which defines a single set of axes as self.axes.
-        sc = MplCanvas(self, width=5, height=4, dpi=100)
-        sc.axes.plot([0,1,2,3,4], [10,1,20,3,40])
+        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
+
+        
+        self.msgF = QMessageBox()
+        self.msgF.setIcon(QMessageBox.Critical)
+        self.msgF.setText("Error in function")
+        self.msgF.setInformativeText('Invalid input function')
+        self.msgF.setWindowTitle("Error")
+       
+        self.msgV = QMessageBox()
+        self.msgV.setIcon(QMessageBox.Critical)
+        self.msgV.setText("Error in value")
+        self.msgV.setInformativeText('Missing min or max value')
+        self.msgV.setWindowTitle("Error")
 
         # Create widgets for the application 
-        plotButton = QPushButton("Plot")
-        plotButton.clicked.connect(self.inputFunction)
-        textFieldFunction = CustomEditText("Enter Function")
-        textFieldMin = CustomEditText("Min Value")
-        textFieldMax = CustomEditText("Max Value")
+        self.plotButton = QPushButton("Plot")
+        self.plotButton.clicked.connect(self.inputFunction)
+        self.textFieldFunction = CustomEditText("Enter Function")
+        self.textFieldMin = CustomEditText("Min Value")
+        self.textFieldMax = CustomEditText("Max Value")
 
         # Horizontal layout elements 
-        layout1.addWidget(textFieldMin)
-        layout1.addWidget(textFieldMax)
-        layout1.addWidget(plotButton)
+        layout1.addWidget(self.textFieldMin)
+        layout1.addWidget(self.textFieldMax)
+        layout1.addWidget(self.plotButton)
 
         # vertical layout elements 
-        layout2.addWidget(textFieldFunction)
+        layout2.addWidget(self.textFieldFunction)
         layout2.addLayout(layout1)
-        layout2.addWidget(sc)
+        layout2.addWidget(self.sc)
         layout2.setAlignment(Qt.AlignTop)
 
         self.setIcon()
@@ -73,7 +91,32 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(appIcon)
     
     def inputFunction(self):
-        pass
+        funcText = self.textFieldFunction.text()
+        self.validate(funcText)
+    
+    def validate(self, expr):
+        
+        try:
+           self.plot(expr)
+        except SympifyError:
+            self.msgF.exec_()
+        except ValueError:
+            self.msgV.exec_()
+
+    def plot(self, expr):
+        min = int(self.textFieldMin.text())
+        max = int(self.textFieldMax.text())
+        x = var('x')
+        f = np.linspace(min, max, LS)
+        y = []
+        self.syExpr = sympify(expr)
+        for i in f:
+            y.append(self.syExpr.subs(x, i))
+        self.sc.axes.cla()
+        self.sc.axes.plot(f, y)
+        self.sc.draw()
+
+
 
 # Customized Edit Text 
 class CustomEditText(QLineEdit):
@@ -103,16 +146,3 @@ window = MainWindow()
 window.show()
 
 app.exec_()
-
-
-
-
-# import ast
-
-# expr = '(x + 5) * 3 ?'
-# try:
-#     ast.parse(expr)
-#     print('valid')
-# except SyntaxError:
-#     print('invalid')
-# https://stackoverflow.com/questions/67264856/how-can-i-validate-a-math-expression-syntax-without-calculate-it
